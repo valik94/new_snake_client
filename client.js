a0bf115131f02af00f0ef6576250b1da58c7e402
Val Hustle <stars@MacBook-Air.local>
2021-12-01 14:16:09 -0500

    initial commit updated client.js and moved the connect and the console log int the client.js file and in play.js only requiring net and client

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+const port = 14736;
+
+// establishes a connection with the game server
+const connect = function () {
+    const conn = net.createConnection({
+      host: "165.227.47.243",
+      port: 50541
+    });
+  
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+  
+    //update connect function to handle incoming data and console log for player
+    conn.on("connect", (data) => {
+        console.log(data)
+      // code that does something when the connection is first established
+    });
+    
+    return conn;
+  };
+
+  module.exports = {connect}
+  
+
---
+++ play.js
@@ -0,0 +1,6 @@
+const net = require("net");
+const {connect} = require("./client")
+
+
+console.log("Connecting ...");
+connect();
