6d48d18577da25a6d87e2d6d25fa2352b5289fa7
Val Hustle <stars@MacBook-Air.local>
2021-12-01 18:26:16 -0500

    changes made to input.js the setupInput function was taken outside and the if statements were written for the handleUserInput as well as the stdin.on was adjusted as it is the eventListener and comments were inputted explaining the raitonal behind the code

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const setupInput = require('./input');
 
 // establishes a connection with the game server
 const connect = function () {
--- input.js
+++ input.js
@@ -1,23 +1,38 @@
 let connection;
 
+//eventhandler manages the event that happens in this case its key binding 'w''a's''d' and ASCII for Ctrl+C
+const handleUserInput =  (data) => { 
+    if (data === '\u0003'){  //'\u0003' = CTRL+C
+    process.exit();
+  }
+  if (data === 'w') { 
+    connection.write('Move: up');
+    console.log("up was pressed: ", data) 
+  }
+  if (data === 'a') {
+    connection.write('Move: left');
+    console.log("left was pressed: ", data)
+  }
+  if (data === 's') {
+    connection.write('Move: down');
+    console.log("down was pressed: ", data)
+  }
+  if (data === 'd') {
+    connection.write('Move: right');
+    console.log("right was pressed: ", data)
+  }
+};
+
 const setupInput = function (conn) {
   connection = conn;
   // setup interface to handle user input from stdin
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
-    stdin.resume();
-  
- 
-    const handleUserInput =  (key) => {
-      if (key === '\u0003'){  //'\u0003' = CTRL+C
-        process.exit();
-      }
-      if (key === '\u0077') {
-        connection.write('Move: up');
-      }
-    };
-    stdin.on("data", handleUserInput);
+    stdin.resume();    
+    
+    //eventlistener awaits for the event to fire
+    stdin.on("data", handleUserInput); //.on is the eventListener, data is the specific event listening for and handleUserInput is the invocation of the eventHandler, which ONLY gets invoked when event fires. 
     return stdin;
   };
 
